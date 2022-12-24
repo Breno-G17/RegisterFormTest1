8c2834d60e4ef60d588af48e558af0f30a9f74b6
Breno Gomez <brenogomez151@gmail.com>
2022-12-24 14:56:16 -0300

    This first update i made in my register form i have removed most of the HTML validation attributes, because i am building up a JS validation system in order to replace those previous validation attributes.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("attempt to push on git numer 1")
+// fetching the HTML elements via DOM
+let lastName = document.querySelector('#last_name')
+let email = document.querySelector('#email-input')
+let password = document.querySelector('#password')
+let confirmPassword = document.querySelector('#confirm-password')
+let checkBox = document.querySelector('#terms-conditions')
+let btn = document.querySelector('#submit-btn')
+let nameAlert = document.querySelector('.name-alert')
+let form = document.querySelector('form')
+let inputs = document.querySelectorAll('input')
+
+function varifyFname() {
+  let firstName = document.querySelector('#first_name');
+
+  if(firstName.value === '' || firstName.value === null) {
+    alert('Please enter your first name');
+    return false;
+  }
+  if(!firstName.value.match(/^[a-zA-Z\-]+$/)) {
+    alert('please enter a valid name');
+    return false;
+  }
+  return true;
+}
